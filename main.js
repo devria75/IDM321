4091cfe419eef88e92283797b5aed279fb08b917
devria75 <devria75@users.noreply.github.com>
2018-03-13 16:11:37 -0400

    more
    everything but the hint is working perfectly

--- main.js
+++ main.js
@@ -26,9 +26,69 @@ function handleFormSubmit(event){
     loadSign(myDate, month, day);
 }
 
+//--------------------------------------------------------------------------------------
+//Event Listeners
+//--------------------------------------------------------------------------------------
 
 form.addEventListener('submit', handleFormSubmit, false);
 
+{
+    //get all wand element ids
+    var wand01 = document.getElementById('wand01');
+    var wand02 = document.getElementById('wand02');
+    var wand03 = document.getElementById('wand03');
+    var wand04 = document.getElementById('wand04');
+    var wand05 = document.getElementById('wand05');
+    var wand06 = document.getElementById('wand06');
+    var wand07 = document.getElementById('wand07');
+    var wand08 = document.getElementById('wand08');
+    var wand09 = document.getElementById('wand09');
+    var wand10 = document.getElementById('wand10');
+    var wand11 = document.getElementById('wand11');
+    var wand12 = document.getElementById('wand12');
+
+    //add event listeners to elements
+    wand01.addEventListener('click', function() {
+        displaySign("Cap");
+    }, false);
+    wand02.addEventListener('click', function() {
+        displaySign("Sag");
+    }, false);
+    wand03.addEventListener('click', function() {
+        displaySign("Sco");
+    }, false);
+    wand04.addEventListener('click', function() {
+        displaySign("Lib");
+    }, false);
+    wand05.addEventListener('click', function() {
+        displaySign("Vir");
+    }, false);
+    wand06.addEventListener('click', function() {
+        displaySign("Leo");
+    }, false);
+    wand07.addEventListener('click', function() {
+        displaySign("Can");
+    }, false);
+    wand08.addEventListener('click', function() {
+        displaySign("Gem");
+    }, false);
+    wand09.addEventListener('click', function() {
+        displaySign("Tau");
+    }, false);
+    wand10.addEventListener('click', function() {
+        displaySign("Ari");
+    }, false);
+    wand11.addEventListener('click', function() {
+        displaySign("Pis");
+    }, false);
+    wand12.addEventListener('click', function() {
+        displaySign("Aqu");
+    }, false);
+}
+
+//--------------------------------------------------------------------------------------
+//Load Sign (sends to Assign and Display functions)
+//--------------------------------------------------------------------------------------
 
 function loadSign(date, month, day){
     //use Assign Zodiac to find which sign it is based on the date
@@ -40,6 +100,9 @@ function loadSign(date, month, day){
     displaySign(mySign);
 }
 
+//--------------------------------------------------------------------------------------
+//Assign Zodiac
+//--------------------------------------------------------------------------------------
 
 function assignZodiac(month, day){
     var whichMonth = month;
@@ -81,47 +144,62 @@ function assignZodiac(month, day){
       return AstroSign;
 }
 
+//--------------------------------------------------------------------------------------
+//Display Sign
+//--------------------------------------------------------------------------------------
 
 function displaySign(sign){
     
     document.getElementById('wandDetails').hidden = false;
     var wandDescription = document.getElementById('wandDescription');
+    var wandImage = document.getElementById('yourWand');
 
     if (sign === "Cap"){
-        //replace wand image spot with actual wand
+        wandImage.src="wands/wand1.png";
         wandDescription.innerHTML = '10 3/4 inch oak wand with a dragon heartstring core';
         document.getElementById('recording01').play();
     } else if (sign === "Sag"){
+        wandImage.src="wands/wand2.png";
         wandDescription.innerHTML = '9 1/2 inch birch wand with a unicorn horn core';
         document.getElementById('recording02').play();
     } else if (sign === "Sco"){
+        wandImage.src="wands/wand3.png";
         wandDescription.innerHTML = '8 1/4 inch redwood wand with a pheonix feather core';
         document.getElementById('recording03').play();
     } else if (sign === "Lib"){
+        wandImage.src="wands/wand4.png";
         wandDescription.innerHTML = '11 3/4 inch ash wand with a hippogriff hair core';
         document.getElementById('recording04').play();
     } else if (sign === "Vir"){
+        wandImage.src="wands/wand5.png";
         wandDescription.innerHTML = '12 1/4 inch hawthorn wand with a veela hair core';
         document.getElementById('recording05').play();
     } else if (sign === "Leo"){
+        wandImage.src="wands/wand6.png";
         wandDescription.innerHTML = '9 1/2 inch cherry wand with a mermaid scale core';
         document.getElementById('recording06').play();
     } else if (sign === "Can"){
+        wandImage.src="wands/wand7.png";
         wandDescription.innerHTML = '10 3/4th inch cedar wand with a thestral tail-hair core';
         document.getElementById('recording07').play();
     } else if (sign === "Gem"){
+        wandImage.src="wands/wand8.png";
         wandDescription.innerHTML = '8 3/4th inch walnut wand with a troll whisker core';
         document.getElementById('recording08').play();
     } else if (sign === "Tau"){
+        wandImage.src="wands/wand9.png";
         wandDescription.innerHTML = '11 1/4th inch rosewood wand with a mermaid scale core';
         document.getElementById('recording09').play();
     } else if (sign === "Ari"){
+        wandImage.src="wands/wand10.png";
         wandDescription.innerHTML = '12 1/2th inch pine wand with a hippogriff hair core';
         document.getElementById('recording10').play();
     } else if (sign === "Pis"){
+        wandImage.src="wands/wand11.png";
         wandDescription.innerHTML = '11 3/4th inch dogwood wand with a dragon heartstring core';
         document.getElementById('recording11').play();
     } else if (sign === "Aqu"){
+        wandImage.src="wands/wand12.png";
         wandDescription.innerHTML = '9 1/4th inch ginko wand with dragon a troll whisker core';
         document.getElementById('recording12').play();
     } else {
